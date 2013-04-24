65f8347e5fd5de1fd7e6e835983331add6d64c66
Facundo Viale <fviale@despegar.com>
2013-04-24 10:53:55 -0300

    Support features for spring 3.1

---
+++ src/main/java/com/bookshelf/client/spring/properties/BookshelfConfigurer.java
@@ -0,0 +1,80 @@
+package com.bookshelf.client.spring.properties;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.bookshelf.client.connector.JerseyConnector;
+import com.bookshelf.client.connector.RESTConnector;
+
+public abstract class BookshelfConfigurer {
+
+    public static final Class<? extends RESTConnector> DEFAULT_REST_CONNECTOR = JerseyConnector.class;
+    public static final String BOOKSHELF_URL = "http://%s/bookshelf/query/%s/%s/%s";
+
+    public static final String HOST_SYSTEM_PROPERTY_KEY = "bookshelf.client.host";
+    public static final String ENVIROMENT_SYSTEM_PROPERTY_KEY = "bookshelf.client.env";
+
+    protected boolean continueWithConnectionErrors = false;
+
+    protected RESTConnector connector;
+
+    protected String serverUrl;
+
+    public BookshelfConfigurer(Class<? extends RESTConnector> restConnector, String bookshelfDomain, String projectName,
+        String moduleName, String enviroment) {
+
+        this.serverUrl = String.format(BOOKSHELF_URL, bookshelfDomain, projectName, moduleName, enviroment);
+
+        try {
+
+            this.getLogger().debug("Creating RESTConnector [" + restConnector.getSimpleName() + "]  for " + this.serverUrl);
+
+            this.connector = restConnector.getConstructor(String.class).newInstance(this.serverUrl);
+        } catch (Exception e) {
+
+            this.getLogger().warn("Cannot establish connection with " + this.serverUrl, e);
+
+            if (!this.continueWithConnectionErrors) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public Map<String, String> getPropertiesFromServer() {
+
+        if (this.connector == null) {
+            return Collections.emptyMap();
+        }
+
+        Map<String, String> properties = Collections.emptyMap();
+
+        try {
+
+            properties = this.connector.get();
+
+            this.getLogger().info("Properties From Server:\n" + properties);
+
+        } catch (Exception e) {
+            if (!this.continueWithConnectionErrors) {
+                this.getLogger().error("Cannot get values from " + this.serverUrl, e);
+                throw new RuntimeException(e.getCause());
+            } else {
+                this.getLogger().warn("Cannot get values from with " + this.serverUrl, e);
+            }
+        }
+
+        return properties;
+    }
+
+    public boolean isContinueWithConnectionErrors() {
+        return this.continueWithConnectionErrors;
+    }
+
+    public void setContinueWithConnectionErrors(boolean continueWithConnectionErrors) {
+        this.continueWithConnectionErrors = continueWithConnectionErrors;
+    }
+
+    public abstract Logger getLogger();
+}
--- src/main/java/com/bookshelf/client/spring/properties/BookshelfPropertySourcePlaceholderConfigurer.java
+++ src/main/java/com/bookshelf/client/spring/properties/BookshelfPropertySourcePlaceholderConfigurer.java
@@ -1,7 +1,8 @@
 package com.bookshelf.client.spring.properties;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.BeansException;
@@ -11,83 +12,83 @@ import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.core.Ordered;
 import org.springframework.core.PriorityOrdered;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
 
 import com.bookshelf.client.connector.RESTConnector;
 
 
 /**
- * 
- * This class is for Spring 3.0 or lower
+ * This class is for Spring 3.1 or grater
  * 
  * @author fviale
  *
  */
-public class BookshelfPropertyPlaceholderConfigurer
+public class BookshelfPropertySourcePlaceholderConfigurer
     extends BookshelfConfigurer
-    implements BeanFactoryPostProcessor, BeanFactoryAware, BeanNameAware, PriorityOrdered {
+    implements BeanFactoryPostProcessor, EnvironmentAware, BeanFactoryAware, BeanNameAware, PriorityOrdered {
 
-    private static final Logger LOGGER = Logger.getLogger(BookshelfPropertyPlaceholderConfigurer.class);
+    private static final Logger LOGGER = Logger.getLogger(BookshelfPropertySourcePlaceholderConfigurer.class);
 
     protected BeanFactory beanFactory;
     protected String beanName;
+    private Environment environment;
 
     protected String fileEncoding = null; // "utf-8";
     protected String nullValue = "NULL";
     private boolean ignoreUnresolvablePlaceholders = true;
     private int systemPropertiesMode = PropertyPlaceholderConfigurer.SYSTEM_PROPERTIES_MODE_OVERRIDE;
 
-    public BookshelfPropertyPlaceholderConfigurer(String projectName, String moduleName) {
+    public BookshelfPropertySourcePlaceholderConfigurer(String projectName, String moduleName) {
         this(DEFAULT_REST_CONNECTOR, projectName, moduleName);
     }
 
-    public BookshelfPropertyPlaceholderConfigurer(String bookshelfDomain, String projectName, String moduleName,
+    public BookshelfPropertySourcePlaceholderConfigurer(String bookshelfDomain, String projectName, String moduleName,
         String enviroment) {
         this(DEFAULT_REST_CONNECTOR, bookshelfDomain, projectName, moduleName, System
             .getProperty(ENVIROMENT_SYSTEM_PROPERTY_KEY) != null ? System.getProperty(ENVIROMENT_SYSTEM_PROPERTY_KEY)
             : enviroment);
     }
 
-    public BookshelfPropertyPlaceholderConfigurer(String bookshelfDomain, String projectName, String moduleName) {
+    public BookshelfPropertySourcePlaceholderConfigurer(String bookshelfDomain, String projectName, String moduleName) {
         this(DEFAULT_REST_CONNECTOR, bookshelfDomain, projectName, moduleName, System
             .getProperty(ENVIROMENT_SYSTEM_PROPERTY_KEY));
     }
 
-    public BookshelfPropertyPlaceholderConfigurer(Class<? extends RESTConnector> restConnector, String projectName,
+    public BookshelfPropertySourcePlaceholderConfigurer(Class<? extends RESTConnector> restConnector, String projectName,
         String moduleName) {
         this(restConnector, System.getProperty(HOST_SYSTEM_PROPERTY_KEY), projectName, moduleName, System
             .getProperty(ENVIROMENT_SYSTEM_PROPERTY_KEY));
     }
 
-    public BookshelfPropertyPlaceholderConfigurer(Class<? extends RESTConnector> restConnector, String bookshelfDomain,
-        String projectName, String moduleName, String enviroment) {
+    public BookshelfPropertySourcePlaceholderConfigurer(Class<? extends RESTConnector> restConnector,
+        String bookshelfDomain, String projectName, String moduleName, String enviroment) {
 
         super(restConnector, bookshelfDomain, projectName, moduleName, enviroment);
     }
 
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        PropertyPlaceholderConfigurer pphc = new PropertyPlaceholderConfigurer();
 
-        // Configure the configurer (!)
-        pphc.setBeanFactory(beanFactory);
-        pphc.setBeanName("##BookshelfPropertyPlaceholderConfigurer##@" + this.beanName);
-        pphc.setIgnoreResourceNotFound(true);
-        pphc.setIgnoreUnresolvablePlaceholders(this.ignoreUnresolvablePlaceholders);
-        pphc.setFileEncoding(this.fileEncoding);
-        pphc.setNullValue(this.nullValue);
-        pphc.setSystemPropertiesMode(this.systemPropertiesMode);
+        if (this.environment instanceof ConfigurableEnvironment) {
 
-        Properties dummyProps = new Properties();
+            Map<String, Object> properties = new HashMap<String, Object>();
 
-        for (Entry<String, String> entry : this.getPropertiesFromServer().entrySet()) {
-            dummyProps.setProperty(entry.getKey(), entry.getValue());
+            for (Entry<String, String> entry : this.getPropertiesFromServer().entrySet()) {
+                properties.put(entry.getKey(), entry.getValue());
+            }
+
+            ((ConfigurableEnvironment) this.environment).getPropertySources().addFirst(
+                new MapPropertySource("localProps", properties));
         }
+    }
 
-        pphc.setProperties(dummyProps);
-
-        // process...
-        pphc.postProcessBeanFactory(beanFactory);
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 
     @Override
